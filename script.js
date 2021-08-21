997661fce1d91c4442680535f4c284663bd5ac3d
Biplab Pati <89262168+Biplab61@users.noreply.github.com>
2021-08-21 08:42:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,15 @@ function portfolioItemDetails(portfolioItem){
     document.querySelector(".pp-body").innerHTML =
     portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
+
+/* -------------------Contact Form Google Sheet--------------- */
+
+            const scriptURL = 'https://script.google.com/macros/s/AKfycbylTFeZ9ioyQXMhViky8NUPw_tchkcoNGwIDIknwiGKIAjpv8AsO8LYWt5AVviOr6_M5w/exec'
+            const form = document.forms['contact-form']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting me..!  I Will Contact You Soon..."))
+                .catch(error => console.error('Error!', error.message))
+            })
